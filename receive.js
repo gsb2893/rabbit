47cc2025aadb4b8e124d3e7867c6d0c7323d35b2
DIR\gabriel.de.s.barbosa <gabriel.de.s.barbosa@accenture.com>
2018-02-28 10:09:55 -0300

    no message

--- receive.js
+++ receive.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env nodejs
-
+//Requisitamos a biblioteca de envio
 var amqp = require('amqplib/callback_api');
-
+//Abrimos a conexão com o RabbitMQ
 amqp.connect('amqp://localhost', function(err, conn) {
+  //Criamos um canal
   conn.createChannel(function(err, ch) {
+    //Definimos o nome da queue que vamos ouvir
     var q = 'hello';
-
+    //Recriamos a queue apenas para ter certeza que ela existe.
+    //Se ela já existir nada acontece
     ch.assertQueue(q, {durable: false});
+    //Dou um log apenas para teste
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+    //Consumo a mensagem, basicamente fico aguardando e quando receber jogo a mensagem no log
     ch.consume(q, function(msg) {
       console.log(" [x] Received %s", msg.content.toString());
     }, {noAck: true});
--- send.js
+++ send.js
@@ -1,16 +1,26 @@
 #!/usr/bin/env nodejs
 
+//Requisitamos a biblioteca de envio
 var amqp = require('amqplib/callback_api');
-
+//Conectamos ao RabbitMQ. No caso esta instalado na máquina local
 amqp.connect('amqp://localhost', function(err, conn) {
+  //Criamos um canal
   conn.createChannel(function(err, ch) {
+    //nome da queue
     var q = 'hello';
+    //mensagem que estará na queue
     var msg = 'Hello World!';
-
+    //Crio a queue, dou um nome a ela e digo que ela não vai ser durável
+    //É importante dizer que você pode dar esse comando quantas vezes
+    //quiser, a query só será criada se nenhuma query com esse nome existir
     ch.assertQueue(q, {durable: false});
-    // Note: on Node 6 Buffer.from(msg) should be used
+    /* Note: on Node 6 Buffer.from(msg) should be used
+    *  Mando a mensagem para dentro da queue com a função sendToQueue
+    */
     ch.sendToQueue(q, new Buffer(msg));
+    //Dou um console log apenas para teste
     console.log(" [x] Sent %s", msg);
   });
+  //Aqui vamos fechar a conexão
   setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
